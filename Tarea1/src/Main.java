a6b406604ff75fc3b2d63b7c4732ab8a5f2a21a0
Alumno Mañana <Alumno Maana@LB-DAM-22.iesluisbraille.es>
2026-09-22 09:17:54 +0200

    Entrega Parcial

--- Tarea1/src/Main.java
+++ Tarea1/src/Main.java
@@ -3,7 +3,7 @@ import java.nio.file.Paths;
 import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter
 public class Main {
 
     public static Clientes darDeAlta(){
