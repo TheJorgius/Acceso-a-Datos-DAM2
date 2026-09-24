b2ce529f1795da30ea414f426afe845af5ab097f
Jorge Lucena Sanz <jorge.lucena.sanz@gmail.com>
2026-09-24 17:01:35 +0200

    To String y arreglo para guardar lectura y imprimir clientes

--- Tarea1.2/src/main/java/org/example/Cliente.java
+++ Tarea1.2/src/main/java/org/example/Cliente.java
@@ -7,8 +7,19 @@ public class Cliente {
     String matricula;
 
     public Cliente(int id, String nombre, String telefono, String matricula) {
-
+        this.id = id;
+        this.nombre = nombre;
+        this.tlfn = telefono;
+        this.matricula = matricula;
     }
 
-    
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", tlfn='" + tlfn + '\'' +
+                ", matricula='" + matricula + '\'' +
+                '}';
+    }
 }
