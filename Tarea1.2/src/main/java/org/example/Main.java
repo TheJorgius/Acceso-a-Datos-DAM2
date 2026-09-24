4c924287c0c8157e3fb006a4e09818b3d7c04704
Jorge Lucena Sanz <jorge.lucena.sanz@gmail.com>
2026-09-24 19:10:01 +0200

    Implementacion copiada del menu al main de anterior version

--- Tarea1.2/src/main/java/org/example/Main.java
+++ Tarea1.2/src/main/java/org/example/Main.java
@@ -2,11 +2,64 @@ package org.example;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
+
+    public static void mostrarMenu() {
+        System.out.println("=== GESTIÓN DE GASOLINERA ===");
+        System.out.println("1. Dar de alta un cliente");
+        System.out.println("2. Listar clientes");
+        System.out.println("3. Buscar clientes");
+        System.out.println("4. Procesar un pago de repostaje");
+        System.out.println("5. Consultar pagos");
+        System.out.println("0. Salir");
+        System.out.println("Opcion:");
+
+
+
+
+    }
+
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         Ficheros F1= new Ficheros();
 
+        do {
+            mostrarMenu();
+            int opcion = sc.nextInt();
+            switch (opcion) {
+                case 1:
+                    Cliente altaCliente = G1.darDeAlta();
+                    
+                    break;
+                case 2:
+                    G1.listarClientes(F1);
+                    break;
+                case 3:
+                    G1.buscarClientes(F1);
+                    break;
+                case 4:
+                    G1.procesarPago(F1);
+                    break;
+                case 5:
+                    G1.consultarPagos(F1);
+                    break;
+                case 0:
+
+                    break;
+                default:
+                    System.out.println("Esa no es una opcion valida selecciona una opcion valida");
+                    try {
+                        F1.crearDirectorioYArchivos();
+                    }catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                    break;
+
+            }
+
+        }while (opcion != 0);
         try {
             System.out.println("Lista de clientes");
             List <Cliente> clientes = F1.leerClientes();
@@ -17,4 +70,5 @@ public class Main {
 
 
     }
+
 }
